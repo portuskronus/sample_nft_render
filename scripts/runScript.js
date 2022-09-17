bf5e80a3aaeca73ee011f09d8d8b50c2125826ee
Portus Kronus <113653361+portuskronus@users.noreply.github.com>
2022-09-17 05:57:32 +0530

    Update runScript.js
    Switched comments for both scripts. Run script with assets first then comment out first const filePath and run the second filePath

--- scripts/runScript.js
+++ scripts/runScript.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const pinFileToIPFS = require('./pinFileToIPFS');
 
-//const filePath = path.join(__dirname, '../assets/beach.png');
-const filePath = path.join(__dirname, '../data/metadata.json');
+const filePath = path.join(__dirname, '../assets/beach.png');
+//const filePath = path.join(__dirname, '../data/metadata.json');
 
 pinFileToIPFS(filePath);
